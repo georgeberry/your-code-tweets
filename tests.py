61da71a8b4fdcb4d5403a3d38d09214503439a03
George Berry <geb97@cornell.edu>
2015-04-06 23:44:04 -0400

    final little fix for the night

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-from tweet_decorator import tweet_me
+from your_code_tweets import tweet_me
 
 
 if __name__ == '__main__':
 
     #as a decorator
-    @tweet_me('@username')
+    @tweet_me('')
     def failure():
         return 'not an integer'/42
 
