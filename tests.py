77ad6434b51a3bfef15301e418f4e34aae8973cc
georgeberry <geb97@cornell.edu>
2015-04-15 13:42:27 -0400

    updated test

--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 from your_code_tweets import tweet_me
 
 #tweets success or failure
-@tweet_me('@georgeberry', 'conf.yaml')
+@tweet_me('@georgeberry', 'conf.json')
 def failure():
     return 'not an integer'/42
 
 #don't tweet on success
-@tweet_me('@georgeberry', credentials='conf.yaml', tweet_success=False)
+@tweet_me('@georgeberry', credentials='conf.json', tweet_success=False)
 def success():
     return 42/42
 
